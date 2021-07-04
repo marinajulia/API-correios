0c0e2ff3bae797c085481df6669a721c0074f10d
marinajulia <marinajulia707@gmail.com>
2021-07-04 15:36:54 -0300

    pasta de carga no banco de dados

--- API correios/Program.cs
+++ API correios/Program.cs
@@ -22,5 +22,5 @@ namespace API_correios
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+    } 
 }
---
+++ Carga no banco de dados correiros/Carga no banco de dados correiros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carga_no_banco_de_dados_correiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
