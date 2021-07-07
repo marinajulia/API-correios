76639efe07771fd24f68b7184c681d87c22b4052
marinajulia <marinajulia707@gmail.com>
2021-07-07 16:13:14 -0300

    inserindo em bairro

--- Carga no banco de dados correiros/Carga no banco de dados correiros/Program.cs
+++ Carga no banco de dados correiros/Carga no banco de dados correiros/Program.cs
@@ -12,7 +12,7 @@ namespace Carga_no_banco_de_dados_correiros
     {
         static void Main(string[] args)
         {
-            CadastrarLocalidade();
+            //CadastrarLocalidade();
             CadastrarBairro();
 
         }
@@ -51,13 +51,11 @@ namespace Carga_no_banco_de_dados_correiros
             }
             catch (Exception)
             {
-
                 Console.WriteLine("\nTabela localidade já inserida");
             }
 
 
         }
-
 
         private static void CadastrarBairro()
         {
@@ -65,7 +63,7 @@ namespace Carga_no_banco_de_dados_correiros
 
             string[] lines = File.ReadAllLines(@"C:\Users\Marina\source\repos\Arquivos Correios\eDNE_Basico\Delimitado\LOG_BAIRRO.TXT", Encoding.UTF7);
 
-            List<LogBairro> bairro = new List<LogBairro>();
+            List<LogBairro> bairros = new List<LogBairro>();
 
             foreach (var line in lines)
             {
@@ -74,9 +72,10 @@ namespace Carga_no_banco_de_dados_correiros
 
                 loc.BAI_NU = Convert.ToInt32(props[0]);
                 loc.UFE_SG = props[1];
-                loc.LogLocalidadeId = Convert.ToInt32(props[2]);
-                loc.BAI_NO = props[3];
-                loc.BAI_NO_ABREV = props[4];
+                loc.LogLocalidadeId = Convert.ToInt32(props[0]);
+                loc.BAI_NO = props[1];
+                loc.BAI_NO_ABREV = props[1];
+                bairros.Add(loc);
 
                 //Console.WriteLine(props[0]);
                 //Console.WriteLine(line);
@@ -84,16 +83,18 @@ namespace Carga_no_banco_de_dados_correiros
             }
             try
             {
-                db.BulkInsert(bairro);
+                db.BulkInsert(bairros);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-
-                Console.WriteLine(e.Message);
+                Console.WriteLine("\nTabela bairro já inserida");
             }
 
 
         }
+
+
+
 
     }
 }
